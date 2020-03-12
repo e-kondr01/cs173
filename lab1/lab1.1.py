08808e96745b2dc0dba5d1256795454804a3e0e8
e-kondr01 <e.kondr01@gmail.com>
2020-03-12 19:21:13 +0300

    lab1.1

--- lab1/lab1.1.py
+++ lab1/lab1.1.py
@@ -1,35 +1,27 @@
+from skimage.color import rgb2gray
 from skimage.io import imread, imsave
+from skimage.util import img_as_ubyte
 
 
 def downgrade(img, levels_n: int = 256):
-    '''Уменьшение кол-ва уровней яркости изображения '''
-    levels = []
-    for i in range(levels_n + 1):
-        levels.append(round(256 / levels_n * i))
-    if 256 - levels[len(levels)-2] < 256 // levels_n * 2:
-        levels[len(levels)-1] = 255
-    print(levels)  # Debug
-    rows = img.shape[0]
-    cols = img.shape[1]
-    for row in range(rows):
-        for col in range(cols):
-            for i in range(len(levels)):
-                if img[row, col][0] < levels[i]:
-                    if levels[i-1] == 0:
-                        img[row, col] = [0, 0, 0]
-                    elif levels[i] == 255:
-                        img[row, col] = [255, 255, 255]
-                    else:
-                        value = (levels[i-1] + levels[i]) // 2
-                        img[row, col] = [value, value, value]
-                    break
-    return img
+    '''Уменьшение кол-ва уровней яркости изображения'''
+    downgraded_img = img // (256 // levels_n)
+    return downgraded_img
+
+
+def restore(img, levels_n: int = 256):
+    '''восстановление изображения для просмотра'''
+    restored_img = img * (256 // levels_n)
+    return restored_img
 
 
 if __name__ == '__main__':
     # img_name = input('Введите название файла с изображением: ')
     img_name = ('C:\cs173\lab1\eye.jpg')
     img = imread(img_name)
+    img_gray = rgb2gray(img)
+    img_one_channel = img_as_ubyte(img_gray)
     levels_n = int(input('Введите желаемое кол-во уровней яркости: '))
-    downgraded_img = downgrade(img, levels_n)
-    imsave(f'downgraded_img_{levels_n}_levels.png', downgraded_img)
+    downgraded_img = downgrade(img_one_channel, levels_n)
+    restored_img = restore(downgraded_img, levels_n)
+    imsave(f'restored_img_{levels_n}_levels.png', restored_img)
---
+++ module1/prokudin-gorsky.py
@@ -0,0 +1,96 @@
+from numpy import dstack, roll
+from skimage.io import imread, imsave
+
+
+def reduce(img, p: float = 0.05):
+    '''Cutting off edges '''
+    rows = img.shape[0]
+    cols = img.shape[1]
+    img = img[round(rows * p): round(rows * (1-p)),
+              round(cols * p): round(cols * (1-p))]
+    return img
+
+
+def find_optimal_shift(img1, img2, px_shift: int = 15):
+    '''Находим оптимальный сдвиг
+    params: img1 - фиксированное изображение
+    img2 - изображение, которое двигаем
+    px_shift - модуль максимального отклонения сдвига'''
+    max_correlation = 0
+    for x in range(-px_shift, px_shift + 1):
+        for y in range(-px_shift, px_shift + 1):
+            shifted_x_img2 = roll(img2, x, axis=0)
+            shifted_img2 = roll(shifted_x_img2, y, axis=1)
+            correlation = (img1 * shifted_img2).sum()
+            if correlation > max_correlation:
+                max_correlation = correlation
+                optimal_shift_x = x
+                optimal_shift_y = y
+                that_image = shifted_img2
+    print(optimal_shift_x, optimal_shift_y)
+    return (optimal_shift_x, optimal_shift_y, that_image)
+
+
+"""
+def find_optimal_shift(img1, img2, px_shift: int = 15):
+    '''Находим оптимальный сдвиг
+    params: img1 - фиксированное изображение
+    img2 - изображение, которое двигаем
+    px_shift - модуль максимального отклонения сдвига'''
+    max_correlation_x = 0
+    for x in range(-px_shift, px_shift + 1):
+        shifted_img2 = roll(img2, x, axis=0)
+        correlation = (img1 * shifted_img2).sum()
+        if correlation > max_correlation_x:
+            max_correlation_x = correlation
+            optimal_shift_x = x
+    max_correlation_y = 0
+    for y in range(-px_shift, px_shift + 1):
+        shifted_img2 = roll(img2, y, axis=1)
+        correlation = (img1 * shifted_img2).sum()
+        if correlation > max_correlation_y:
+            max_correlation_y = correlation
+            optimal_shift_y = y
+    print(optimal_shift_x, optimal_shift_y)
+    return (optimal_shift_x, optimal_shift_y)
+"""
+
+
+def align(img, g_coord):
+    row_g, col_g = g_coord
+
+    '''getting separate r, g, b channels '''
+    rows = img.shape[0]
+    cols = img.shape[1]
+    end_correct = img.shape[0] % 3  # for same height
+    uncut_b = img[0: rows // 3, :]
+    uncut_g = img[rows // 3: rows // 3 * 2, :]
+    uncut_r = img[rows // 3 * 2: img.shape[0] - end_correct, :]
+    r = reduce(uncut_r)
+    g = reduce(uncut_g)
+    b = reduce(uncut_b)
+    imsave('r.png', r)
+    imsave('g.png', g)
+    imsave('b.png', b)
+    no_shift = dstack((r, g, b))
+    imsave('no_shift.png', no_shift)
+
+    '''moving channels '''
+    blue_on_green = find_optimal_shift(img1=g, img2=b)
+    red_on_green = find_optimal_shift(img1=g, img2=r)
+    row_b = row_g + blue_on_green[0] - rows // 3
+    col_b = col_g + blue_on_green[1]
+    row_r = row_g + red_on_green[0] + rows // 3
+    col_r = col_g + red_on_green[1]
+
+    shifted_b = blue_on_green[2]
+    shifted_r = red_on_green[2]
+    test_image = dstack((shifted_r, g, shifted_b))
+    imsave('test_image.png', test_image)
+
+    return (row_b, col_b), (row_r, col_r)
+
+
+if __name__ == '__main__':
+    img = imread("C:\cs173\module1\\00.png")
+    print(align(img, (508, 237)))
