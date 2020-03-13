76f278bd3a6dbbddc53162f8ff36ab9e739f883d
e-kondr01 <e.kondr01@gmail.com>
2020-03-13 13:13:11 +0300

    stepik_improved

--- module1/prokudin-gorsky.py
+++ module1/prokudin-gorsky.py
@@ -1,5 +1,6 @@
 from numpy import dstack, roll
 from skimage.io import imread, imsave
+from skimage.util import img_as_float, img_as_ubyte
 
 
 def reduce(img, p: float = 0.05):
@@ -31,31 +32,6 @@ def find_optimal_shift(img1, img2, px_shift: int = 15):
     return (optimal_shift_x, optimal_shift_y, that_image)
 
 
-"""
-def find_optimal_shift(img1, img2, px_shift: int = 15):
-    '''Находим оптимальный сдвиг
-    params: img1 - фиксированное изображение
-    img2 - изображение, которое двигаем
-    px_shift - модуль максимального отклонения сдвига'''
-    max_correlation_x = 0
-    for x in range(-px_shift, px_shift + 1):
-        shifted_img2 = roll(img2, x, axis=0)
-        correlation = (img1 * shifted_img2).sum()
-        if correlation > max_correlation_x:
-            max_correlation_x = correlation
-            optimal_shift_x = x
-    max_correlation_y = 0
-    for y in range(-px_shift, px_shift + 1):
-        shifted_img2 = roll(img2, y, axis=1)
-        correlation = (img1 * shifted_img2).sum()
-        if correlation > max_correlation_y:
-            max_correlation_y = correlation
-            optimal_shift_y = y
-    print(optimal_shift_x, optimal_shift_y)
-    return (optimal_shift_x, optimal_shift_y)
-"""
-
-
 def align(img, g_coord):
     row_g, col_g = g_coord
 
@@ -63,29 +39,29 @@ def align(img, g_coord):
     rows = img.shape[0]
     cols = img.shape[1]
     end_correct = img.shape[0] % 3  # for same height
+    img = img_as_float(img)
     uncut_b = img[0: rows // 3, :]
     uncut_g = img[rows // 3: rows // 3 * 2, :]
     uncut_r = img[rows // 3 * 2: img.shape[0] - end_correct, :]
     r = reduce(uncut_r)
     g = reduce(uncut_g)
     b = reduce(uncut_b)
-    imsave('r.png', r)
-    imsave('g.png', g)
-    imsave('b.png', b)
-    no_shift = dstack((r, g, b))
-    imsave('no_shift.png', no_shift)
 
     '''moving channels '''
     blue_on_green = find_optimal_shift(img1=g, img2=b)
     red_on_green = find_optimal_shift(img1=g, img2=r)
+
+    '''finding coordinates '''
     row_b = row_g + blue_on_green[0] - rows // 3
     col_b = col_g + blue_on_green[1]
     row_r = row_g + red_on_green[0] + rows // 3
     col_r = col_g + red_on_green[1]
 
+    '''testing visuals '''
     shifted_b = blue_on_green[2]
     shifted_r = red_on_green[2]
     test_image = dstack((shifted_r, g, shifted_b))
+    test_image = img_as_ubyte(test_image)
     imsave('test_image.png', test_image)
 
     return (row_b, col_b), (row_r, col_r)
