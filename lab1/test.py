4dac018ccdfee7c99619fee9f0f7fd94b01a8c9c
e-kondr01 <e.kondr01@gmail.com>
2020-04-02 21:33:12 +0300

    small update

--- lab1/test.py
+++ lab1/test.py
@@ -1,30 +1,21 @@
-from numpy import histogram
-from skimage.io import imread, imshow, imsave
-from skimage.util import img_as_float, img_as_ubyte
-from numpy import dstack
+from numpy import dstack, histogram
+from skimage.io import imread, imsave
 
 
-img = imread('.\\module2\landscape.png')
+img = imread('./module2/landscape.png')
 values, bin_edges = histogram(img, bins=range(257))
 values_list = list(values)
 x = img.shape[0]
 y = img.shape[1]
 size = x * y
 
-
-def cdf(values_list, bright_num: int):
-    """
-    функция распределения
-    bright_num:  кол-во уровей яркости"""
-    return sum(values_list[:bright_num])
-
-
 for value in values_list:
     if value:
         cdf_min = value
+        break
 
 for i in range(x):
     for j in range(y):
-        img[i][j] = round((cdf(values_list, img[i][j]) - cdf_min) * 255 / (size - 1))
+        img[i][j] = round((sum(values_list[:img[i][j]+1]) - cdf_min) * 255 / (size - 1))
 
-imsave('cdf.png', img)
+imsave('out_img.png', img)
