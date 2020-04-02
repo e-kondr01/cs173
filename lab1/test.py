4a76c6bc7a7265602e302746b0c895a66461a668
e-kondr01 <e.kondr01@gmail.com>
2020-04-02 18:43:53 +0300

    stepik_module2 done

--- lab1/test.py
+++ lab1/test.py
@@ -1,34 +1,30 @@
+from numpy import histogram
 from skimage.io import imread, imshow, imsave
+from skimage.util import img_as_float, img_as_ubyte
+from numpy import dstack
 
 
-def compare_colour(pixel, border_colour):
-    return True if (pixel[0] == border_colour[0] and
-                    pixel[1] == border_colour[1] and
-                    pixel[2] == border_colour[2]) else False
+img = imread('.\\module2\landscape.png')
+values, bin_edges = histogram(img, bins=range(257))
+values_list = list(values)
+x = img.shape[0]
+y = img.shape[1]
+size = x * y
 
 
-img = imread('C:\cs173\lab1\\tiger-border.png')
-red, green, blue = img[0, 0]
-border_colour = [red, green, blue]
-x, y = img.shape[0], img.shape[1]
-start_left = 0
-count_left = 1
-while compare_colour(img[x//2, start_left + 1], border_colour):
-    count_left += 1
-    start_left += 1
-start_top = 0
-count_top = 1
-while compare_colour(img[start_top + 1, y//2], border_colour):
-    count_top += 1
-    start_top += 1
-start_right = y
-count_right = 0
-while compare_colour(img[x//2, start_right - 1], border_colour):
-    count_right += 1
-    start_right -= 1
-start_bot = x
-count_bot = 0
-while compare_colour(img[start_bot - 1, y//2], border_colour):
-    count_bot += 1
-    start_bot -= 1
-print(count_left, count_top, count_right, count_bot)
+def cdf(values_list, bright_num: int):
+    """
+    функция распределения
+    bright_num:  кол-во уровей яркости"""
+    return sum(values_list[:bright_num])
+
+
+for value in values_list:
+    if value:
+        cdf_min = value
+
+for i in range(x):
+    for j in range(y):
+        img[i][j] = round((cdf(values_list, img[i][j]) - cdf_min) * 255 / (size - 1))
+
+imsave('cdf.png', img)
